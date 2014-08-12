0987fd737492f7d5412924077ba7e9d7aa22214c
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-08-12 21:43:07 +0200

    Adds addition & multiplication classes

--- src/comp/mod.rs
+++ src/comp/mod.rs
@@ -1,3 +1,54 @@
+use std::fmt::Show;
+use std::fmt::Formatter;
+use std::fmt::Result;
+
 pub struct Number {
     pub value: int
+}
+
+impl Number {
+    pub fn new(value: int) -> Number {
+        Number{ value: value }
+    }
+}
+
+// implement Show trait for Number
+impl Show for Number {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+pub struct Add {
+    pub left: Number,
+    pub right: Number
+}
+
+impl Add {
+    pub fn new(left: Number, right: Number) -> Add {
+        Add{ left: left, right: right }
+    }
+}
+
+impl Show for Add {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{0} + {1}", self.left, self.right)
+    }
+}
+
+pub struct Multiply {
+    pub left: Number,
+    pub right: Number
+}
+
+impl Multiply {
+    pub fn new(left: Number, right: Number) -> Multiply {
+        Multiply{ left: left, right: right }
+    }
+}
+
+impl Show for Multiply {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{0} * {1}", self.left, self.right)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ mod comp;
 
 fn main() {
     use comp::Number;
+    use comp::Add;
+    use comp::Multiply;
 
+    let add  = Add::new(Number::new(1), Number::new(4));
+    let mult = Multiply::new(Number::new(4), Number::new(3));
+    let comb = Add::new(
+        Multiply::new(Number::new(5), Number::new(10)),
+        Multiply::new(Number::new(3), Number::new(4)),
+    )
 
-    let number = Number{ value: 2 };
-    println!("Number is: {0}", number.value);
+    println!("Addition: {0}", add);
+    println!("Multiplication: {0}", mult);
+    println!("Combination: {0}", comb);
 }
