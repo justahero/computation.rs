1f0950a8264ad365254bb8153bf563a050bf8cda
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-08-13 21:11:56 +0200

    Adds LessThan expression

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,32 @@ mod smallstep;
 fn main() {
     use smallstep::Number;
     use smallstep::Boolean;
+    use smallstep::LessThan;
     use smallstep::Add;
     use smallstep::machine::Machine;
     use smallstep::Multiply;
 
-    let number = Number::new(100);
-    let attrib = Boolean::new(false);
-    let add  = Add::new(Number::new(1), Number::new(4));
-    let mult = Multiply::new(Number::new(4), Number::new(3));
-    let comb = Add::new(
-        Multiply::new(Number::new(5), Number::new(10)),
-        Multiply::new(Number::new(3), Number::new(4)),
-    );
+    println!("Number: {}", Number::new(100));
+    println!("Boolean: {}", Boolean::new(false));
 
-    let machine = Machine::new(comb.clone());
-
-    println!("Number: {}", number);
-    println!("Number reducable: {}", number.reducable());
-    println!("Boolean: {}", attrib);
-    println!("Boolean reducable: {}", attrib.reducable());
+    let add = Add::new(Number::new(1), Number::new(4));
     println!("Addition: {0}", add);
-    println!("Addition reducable: {}", add.reducable());
-    println!("Multiplication: {0}", mult);
-    println!("Multiplication reducable {0}", mult.reducable());
 
+    let mult = Multiply::new(Number::new(4), Number::new(3));
+    println!("Multiplication: {0}", mult);
+
+    let machine = Machine::new(
+        Add::new(
+            Multiply::new(Number::new(5), Number::new(10)),
+            Multiply::new(Number::new(3), Number::new(4)),
+        )
+    );
     println!("Running Machine")
     machine.run();
+
+    let comparison = Machine::new(
+        LessThan::new(Number::new(10), Number::new(20)),
+    );
+    println!("LessThan")
+    comparison.run();
 }
--- src/smallstep/mod.rs
+++ src/smallstep/mod.rs
@@ -7,9 +7,10 @@ pub mod machine;
 #[deriving(Clone)]
 pub enum Node {
     Number(int),
-    Boolean(bool),
     Add(Box<Node>, Box<Node>),
-    Multiply(Box<Node>, Box<Node>)
+    Multiply(Box<Node>, Box<Node>),
+    Boolean(bool),
+    LessThan(Box<Node>, Box<Node>),
 }
 
 impl Node {
@@ -23,7 +24,6 @@ impl Node {
     pub fn value(&self) -> int {
         match *self {
             Number(v)  => { v },
-            // Boolean(v) => { v },
             _ => fail!("Type has no value: {}", *self)
         }
     }
@@ -47,6 +47,15 @@ impl Node {
                     Number(l.value() * r.value())
                 }
             }
+            LessThan(ref l, ref r) => {
+                if l.reducable() {
+                    LessThan(box l.reduce(), r.clone())
+                } else if r.reducable() {
+                    LessThan(l.clone(), box r.reduce())
+                } else {
+                    Boolean(l.value() < r.value())
+                }
+            }
             _ => fail!("Non reducable type found: {}", *self)
         }
     }
@@ -56,9 +65,10 @@ impl Show for Node {
     fn fmt(&self, f: &mut Formatter) -> Result {
         match *self {
             Number(value)          => write!(f, "{}", value),
-            Boolean(value)         => write!(f, "{}", value),
             Add(ref l, ref r)      => write!(f, "{0} + {1}", l, r),
-            Multiply(ref l, ref r) => write!(f, "{0} * {1}", l, r)
+            Multiply(ref l, ref r) => write!(f, "{0} * {1}", l, r),
+            Boolean(value)         => write!(f, "{}", value),
+            LessThan(ref l, ref r) => write!(f, "{0} < {1}", l, r),
         }
     }
 }
@@ -70,16 +80,6 @@ pub struct Number {
 impl Number {
     pub fn new(value: int) -> Node {
         Number(value)
-    }
-}
-
-pub struct Boolean {
-    pub value: bool
-}
-
-impl Boolean {
-    pub fn new(value: bool) -> Node {
-        Boolean(value)
     }
 }
 
@@ -102,5 +102,26 @@ pub struct Multiply {
 impl Multiply {
     pub fn new(left: Node, right: Node) -> Node {
         Multiply(box left, box right)
+    }
+}
+
+pub struct Boolean {
+    pub value: bool
+}
+
+impl Boolean {
+    pub fn new(value: bool) -> Node {
+        Boolean(value)
+    }
+}
+
+pub struct LessThan {
+    pub left: Node,
+    pub right: Node,
+}
+
+impl LessThan {
+    pub fn new(left: Node, right: Node) -> Node {
+        LessThan(box left, box right)
     }
 }
