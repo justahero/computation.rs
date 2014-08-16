628318cd4f71c18fc3aa2e5f0529039a45b6d5a7
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-08-16 13:08:31 +0200

    Adds an insert function to Environment

--- src/smallstep/environment.rs
+++ src/smallstep/environment.rs
@@ -21,6 +21,10 @@ impl Environment {
             None => fail!("Variable {} not found", name)
         }
     }
+
+    pub fn insert(&mut self, name: String, node: Box<Node>) {
+        self.vars.insert(name, node);
+    }
 }
 
 #[test]
@@ -29,4 +33,12 @@ fn test_add_variable_to_environment() {
     env.add("x".to_string(), Node::number(1));
     assert_eq!(1, env.get("x".to_string()).value());
     assert_eq!("1".to_string(), env.get("x".to_string()).to_string());
+}
+
+#[test]
+fn test_insert_variable_to_environment() {
+    let mut env = Environment::new();
+    env.add("x".to_string(), Node::number(2));
+    env.insert("x".to_string(), Node::number(4));
+    assert_eq!(4, env.get("x".to_string()).value());
 }
